579b06ce8cf6822dfb1982b1c90e0f3fcdae7faf
vitor20022003 <vitorm4123@gmail.com>
2020-11-19 22:34:02 -0300

    alteracoes feitas

--- exercicio3/Program.cs
+++ exercicio3/Program.cs
@@ -8,38 +8,45 @@ namespace exercicio3
         {
             // Fazer um programa que leia e valide as seguintes informações:
 
+                    string nome;
                 /*1 Nome: diferente de vazio;*/
-                string nome;
+                
+                do
+                {   
+                    Console.Write("digite o nome: ");
+                    nome = Console.ReadLine();
+                }
+                while (nome == "" || nome == " ");
+                
 
-                Console.Write("digite o nome: ");
-                nome = Console.ReadLine();
-                while (nome == "")
-                {
-                    Console.Write("error, tente novamente!: ");
-                    break;
-                }
                 /*2 Idade: entre 0 e 150;*/
-                Console.Write("digite a idade entre 0 e 150: ");
-                int idade = int.Parse(Console.ReadLine());
-                while(!(idade >0 && idade <150)){
-                    Console.Write("error, tente novamente!: ");
-                    break;
-                }
-                /*3 Salário: maior que zero;*/
-                Console.Write("digite um salário maior que zero: ");
-                float salario = float.Parse(Console.ReadLine());
-                while(!(salario >0)){
-                    Console.Write("error, tente novamente!: ");
-                    break;
-                }
-                /* 4 Estado Civil: 's'(solteiro(a)), 'c'(casado(a)), 'v'(viuvo(a)), 'd'(divorciado(a)); */
-                Console.Write("'s'(solteiro(a)), 'c'(casado(a)), 'v'(viuvo(a)), 'd'(divorciado(a)): ");
-                string resposta = Console.ReadLine();
-                while (!(resposta == "s" || resposta == "c" || resposta == "v" || resposta == "d"))
+                    int idade;
+                do
                 {
-                    Console.Write("error, tente novamente!: ");
-                    break;
+                    Console.Write("digite a idade entre 0 e 150: ");
+                    idade = int.Parse(Console.ReadLine());
                 }
+                while(!(idade >0 || idade <150));
+            
+
+                /*3 Salário: maior que zero;*/
+                float salario;
+                do
+                {
+                    Console.Write("digite um salário maior que zero: ");
+                    salario = float.Parse(Console.ReadLine());
+                }
+                while(!(salario >0));
+
+                /* 4 Estado Civil: 's'(solteiro(a)), 'c'(casado(a)), 'v'(viuvo(a)), 'd'(divorciado(a)); */
+                string resposta;
+                do{
+                    Console.Write("'s'(solteiro(a)), 'c'(casado(a)), 'v'(viuvo(a)), 'd'(divorciado(a)): ");
+                    resposta = Console.ReadLine();
+                }
+                while (!(resposta == "s" || resposta == "c" || resposta == "v" || resposta == "d"));
+
+                Console.WriteLine("dados aceitos!");
         }
     }
 }
